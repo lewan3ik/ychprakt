b7f9536db8f40607e994176f1ed26ba731b0dfce
Alex3455755 <alexeigyll@gmail.com>
2025-05-29 02:59:32 +0500

    сделал программу дисцыплин

--- controllers/programDisciplineController.php
+++ controllers/programDisciplineController.php
@@ -32,4 +32,72 @@ $disArray = array_map(function($elem) {
     echo json_encode(['error' => 'Failed to fetch students', 'details' => $e->getMessage()]);
 }
 }
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['action'])) {
+    $input = json_decode(file_get_contents("php://input"), true);
+    if($_GET['action'] === 'add'){
+        try {
+        $newGroup = new programDisciplineContext(['ID'=>null,'Topic'=> $input['topic'],
+    'ClassType'=>$input['classType'],'Hours' =>$input['hours'],'DisciplineID'=>$input['disciplineId']]);
+        $newGroup->add();
+        $resp = [
+            "success" => true,
+            "message" => "Группа успешно добавлена",
+            "data" => $newGroup->DisciplineID
+        ];
+        echo json_encode($resp);
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => false,
+            'error' => 'Ошибка при добавлении группы',
+            'details' => $e->getMessage()
+        ]);
+    }
+    exit();
+    }
+    if($_GET['action'] === 'update'){
+        try {
+        $newGroup = new programDisciplineContext(['ID'=>$input['id'],'Topic'=> $input['topic'],
+    'ClassType'=>$input['classType'],'Hours' =>$input['hours'],'DisciplineID'=>$input['disciplineId']]);
+        $newGroup->update();
+        $resp = [
+            "success" => true,
+            "message" => "Группа успешно добавлена",
+            "data" => $newGroup->ID
+        ];
+        echo json_encode($resp);
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => false,
+            'error' => 'Ошибка при добавлении группы',
+            'details' => $e->getMessage()
+        ]);
+    }
+    exit();
+    }
+    if($_GET['action'] ==='del'){
+        try {
+            $id = $input['id'];
+            $res = programDisciplineContext::delete($id);
+        $resp = [
+            "success" => true,
+            "message" => "Группа успешно добавлена",
+            "data" => $id,
+            "res" => $res
+        ];
+        echo json_encode($resp);
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo json_encode([
+            'success' => false,
+            'error' => 'Ошибка при добавлении группы',
+            'details' => $e->getMessage()
+        ]);
+    }
+    exit();
+    }
+}
 ?>
