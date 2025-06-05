7feb45652c6d979d2ef6ed17832de78f360bd1f0
Alex3455755 <alexeigyll@gmail.com>
2025-06-05 23:01:50 +0500

    фильтрация добавлена

--- connection/connection.php
+++ connection/connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '12345');
 define('DB_NAME', 'UniversityDB');
 
 class Connection {
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/classes/markContext.php';
-
-// Получаем данные из контекстов
-
-markContext::delete(2);
-?>
